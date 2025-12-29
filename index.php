87fbd2a671db14d5951c3135328621adbbdc0966
Louis <louis.ducourneau@gmail.com>
2025-12-29 22:06:58 +0100

    page index et style

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
-<?php
-    //HomePage : doit proposer un moyen de se connecter.
-    //doit aussi proposer de voir les posts des gens.
-    echo "Hello world";
+<!DOCTYPE html>
+<html>
+<head>
+<title>mini-social</title>
+<LINK rel="styleAcceuil" type="text/css" href="lien_vers_la_feuille"> 
+</head>
+ 
+<body>
+<div class="container1">
+  <p>acceder aux messages</p>
+</div>
+<div class="container2">
+  <p>voici les posts</p>
+</div>
+<div class="container3">
+  <p>voici log ou post</p>
+</div>
+</body>
 
-
-?>
+</html>
